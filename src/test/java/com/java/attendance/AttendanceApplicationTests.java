b3f5f28bd721088fbdd37e84a90574c1c8475762
vchandra22 <vincentchaandra@gmail.com>
2024-12-19 14:26:13 +0700

    feat: initialize & creating boilerplate spring application

---
+++ src/test/java/com/java/attendance/AttendanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.attendance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
